2942f16fefdc30de3795b1d6e0e72733f6bb7075
Ruslan Nasyrov <rilsi222005@gmail.com>
2026-01-28 14:15:52 +0300

    added_a_function_restore

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,11 @@ from db.models import init_db
 from services.tasks import (
     create_task,
     get_all_tasks,
+    get_deleted_tasks,
     mark_task_done,
     mark_task_notdone,
-    delete_task
+    delete_task,
+    restore_task
 )
 
 app = Flask(__name__)
@@ -14,6 +16,11 @@ app = Flask(__name__)
 def index():
     tasks = get_all_tasks(include_done=True)
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/deleted")
+def deleted_tasks():
+    deleted_tasks = get_deleted_tasks(include_done=True)
+    return render_template("deleted_tasks.html", deleted_tasks=deleted_tasks)
 
 @app.route("/add", methods=["POST"])
 def add_task():
@@ -36,6 +43,11 @@ def notdone(task_id):
 def delete(task_id):
     delete_task(task_id)
     return redirect(url_for("index"))
+
+@app.route("/deleted/restore/<int:task_id>")
+def restore(task_id):
+    restore_task(task_id)
+    return redirect(url_for("deleted_tasks"))
 
 if __name__ == "__main__":
     init_db()
--- backend/services/tasks.py
+++ backend/services/tasks.py
@@ -38,6 +38,26 @@ def get_all_tasks(include_done=True):
     return tasks
 
 
+def get_deleted_tasks(include_done=True):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if include_done:
+        cursor.execute("""
+            SELECT * FROM tasks
+            WHERE deleted_at IS NOT NULL
+        """)
+    else:
+        cursor.execute("""
+            SELECT * FROM tasks
+            WHERE status = 'todo' AND deleted_at IS NOT NULL
+        """)
+
+    deleted_tasks = cursor.fetchall()
+    conn.close()
+    return deleted_tasks
+
+
 def mark_task_done(task_id):
     conn = get_connection()
     cursor = conn.cursor()
@@ -99,11 +119,16 @@ def restore_task(task_id):
     conn = get_connection()
     cursor = conn.cursor()
 
+    print(f"DEBUG: Marking task {task_id} as restore_task")
+
     cursor.execute("""
         UPDATE tasks
         SET deleted_at = NULL
-        WHERE id = ?
+        WHERE id = ? AND deleted_at IS NOT NULL
     """, (task_id,))
+
+    rows_updated = cursor.rowcount
+    print(f"DEBUG: Updated {rows_updated} rows")
 
     conn.commit()
     conn.close()
